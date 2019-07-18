0675e593ba269d83c91025e9f318cafb56a18d03
mtd <mtdolaj@gmail.com>
2019-07-18 12:03:42 +0100

    Progress

--- js/main.js
+++ js/main.js
@@ -7,4 +7,24 @@ console.log(password);
 var pass1 = document.querySelector("#pass1").value;
 var pass2 = document.querySelector("#pass2").value;
 
-// function doesPassMatch()
+function doesPassMatch() {
+	if (pass1 == pass2) {
+		console.log("ready to go!");
+	} else {
+		console.log("nope!");
+	}
+}
+
+doesPassMatch();
+
+var database = [
+	{
+		username: "",
+		password: ""
+	}
+]
+
+database.username = user;
+
+var user = username;
+console.log(databsae.username);
