d3f1b70f89814ca9dfa6c684d1e0f4b95b185650
mtd <mtdolaj@gmail.com>
2019-07-18 11:50:58 +0100

    Making progress

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-var username = document.queryselector("input");
+var username = document.querySelector("#name").value;
 console.log(username);
+var surname = document.querySelector("#sname").value;
+console.log(surname);
+var password = document.querySelector(".pass").value;
+console.log(password);
+var pass1 = document.querySelector("#pass1").value;
+var pass2 = document.querySelector("#pass2").value;
+
+// function doesPassMatch()
